fc21a90a17a8b830bb421fe30d395f81048785fb
Jarmo Habakuk <jarmhab@greeny.cs.tlu.ee>
2016-02-17 09:57:44 +0200

    counter

--- app.js
+++ app.js
@@ -10,20 +10,69 @@
     }
     Moosipurk.instance = this; //this viitab moosipurgile
 
+    this.routes = Moosipurk.routes;
+
+
     console.log(this);
     //console.log('moosipurgi sees');
 
     //KÕIK MUUTJUJAD, mis on üldised ja muudetavad
+    this.currentRoute = null; //hoian meeles, mis lehel olen
+    this.interval = null;
+
     this.click_count = 0;
 
     this.init();
   };
 
+  //kirjeldatud kõik lehed
+  Moosipurk.routes = {
+    "home-view": {
+      render: function(){
+      //käivitan siis kui jõuan lehele
+      console.log('JS avalehel');
+
+      // kui olemas, teen nulliks
+        if(this.interval){ clearInterval(this.interval); }
+
+      var seconds = 0;
+      //setInterval(fn, ms); function(){}
+      this.interval = window.setInterval(function(){
+        seconds++;
+        document.querySelector('#counter').innerHTML = seconds;
+      }, 1000); //iga 1000 ms tagant käivitub
+
+      }
+    },
+    "list-view": {
+      render: function(){
+        console.log('JS loendi lehel');
+      }
+    },
+    "manage-view": {
+      render: function(){
+        console.log('JS halduse lehel');
+      }
+    }
+
+  };
   //kõik moosipurgifunktsioonid tulevad siia sisse
   Moosipurk.prototype = {
     init: function(){
       console.log('rakendus käivitus');
       //siia tuleb esialgne loogika
+
+
+
+      window.addEventListener('hashchange', this.routeChange.bind(this));
+      //vaatan, mis lehel olen, kui ei ole hashi lisan avalehe
+      console.log(window.location.hash);
+      if(!window.location.hash){
+        window.location.hash = "home-view";
+      }else{
+        this.routeChange();
+      }
+
       //hakka kuulama hiireklõpse
       this.bindMouseEvents();
     },
@@ -34,6 +83,22 @@
       //console.log(event);
       this.click_count++;
       console.log(this.click_count);
+    },
+    routeChange: function(event){
+      //console.log('>>>>>' + window.location.hash);
+
+      this.currentRoute = window.location.hash.slice(1);
+
+      //kas leht on olemas
+      if(this.routes[this.currentRoute]){
+        console.log('>>>>>' + this.currentRoute);
+        //käivitan selle lehe jaoks ettenähtud js
+        this.routes[this.currentRoute].render();
+      }else{
+        //404?
+        console.log('404');
+        window.location.hash = 'home-view';
+      }
     }
 
   };
